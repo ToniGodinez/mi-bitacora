a20dee60ee2b05552e1537aa33ba9b1ba8ec121e
tonyonly <godinez8977@gmail.com>
2025-09-02 17:40:44 -0600

    �� Mejora visual: Usar ícono oficial de TMDB para actores sin foto
    - Reemplazar placeholder genérico por ícono SVG oficial de TMDB
    - Mejorar consistencia visual en sección de reparto
    - URL: themoviedb.org/assets/glyphicons-basic-4-user-grey

--- src/pages/RecommendMovie.jsx
+++ src/pages/RecommendMovie.jsx
@@ -413,7 +413,7 @@ const RecommendMovie = () => {
                       <img 
                         src={actor.profile_path 
                           ? `https://image.tmdb.org/t/p/w185${actor.profile_path}` 
-                          : '/api/placeholder/100/150'
+                          : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg'
                         }
                         alt={actor.name}
                         className="actor-photo"
